801672549932804479b25656730081606313b55a
Arthur Tirado <58999188+Tecnistico@users.noreply.github.com>
2023-04-17 17:49:33 -0400

    product

--- html/product-view.php
+++ html/product-view.php
@@ -7,6 +7,29 @@
     <title>Product</title>
 </head>
 <body>
-    TEST
+    <nav>
+
+    </nav>
+    <main>
+        <div class="flex">
+            <img src="<? $img ?>" alt="<? $product ?>">
+        </div>
+        <div class="product-name">
+            <? $name ?>
+        </div>
+        <div class="product-description">
+            <? $description ?>
+        </div>
+        <div class="price">
+            <? $price ?>
+        </div>
+        <form action="product.php" method="get">
+            <input type="number">
+            <input type="submit" value="Ajouter au panier">
+        </form>
+    </main>
+    <footer>
+        <div>Copyright 2023 Arthur Tirado et Hugo Larochele</div>
+    </footer>
 </body>
 </html>

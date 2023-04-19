59ca97da6c458cf77f2936207aadf4d939c0a862
GlucoseLeGlock <minigo17@outlook.com>
2023-04-19 15:09:53 -0400

    header fait

--- html/index-view.php
+++ html/index-view.php
@@ -4,17 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Acceuil</title>
 </head>
 <body>
-    <header>
-        <img src="img/brand.svg" alt="logo de la compagnie">
-        <a href="cart.php"><img src="img/cart.svg"></a>
-        <?php if(is_empty_or_blank($user)) {?>
-        <a href="sign-in.php">Connexion</a>
-        <?php } else { ?>
-            <a href="sign-out.php">Se déconnecter</a>
-        <?php } ?>
+    <header class="flex-space-between bottom-border">
+        <a href="index.php"><img src="img/brand.svg" alt="logo de la compagnie"></a>
+        <div class="flex-space-between">
+            <a href="cart.php"><img class="shopping-cart" src="img/cart.svg"></a>
+            <?php if(is_empty_or_blank($user)) {?>
+                <a href="sign-in.php" class="blue-button">Connexion</a>
+            <?php } else { ?>
+                <a href="sign-out.php">Se déconnecter</a>
+            <?php } ?>
+        </div>
     </header>
     <main>
 
--- index.php
+++ index.php
@@ -1,2 +1,9 @@
 <?php
-    require_once __DIR__."/html/index-view.php";
+
+$user = "";
+
+function is_empty_or_blank($object) {
+    return trim($object) === "";
+}
+
+require_once __DIR__."/html/index-view.php";

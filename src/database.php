87e453c712d6bf2ea6f81bb170c466ea04f4aa3e
GlucoseLeGlock <minigo17@outlook.com>
2023-04-19 15:37:22 -0400

    database/productdao

--- src/database.php
+++ src/database.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 function connect_db() : PDO {
     try {
         return new PDO("mysql:host=localhost;dbname=baie_ourson", "student", "ubuntu", array(
@@ -8,17 +8,6 @@ function connect_db() : PDO {
         ));
     } catch (PDOException $e) {
         exit("Unable to connect to database :{$e->getMessage()}");
-    }
-}
-
-function return_product(PDO $db, string $sku) : bool {
-    try {
-        $statement = $db->prepare("SELECT name, description, price FROM product WHERE sku = ?");
-        $statement->execute([$sku]);
-        return $statement->fetch();
-   
-    } catch (PDOException $e) {
-        return false;
     }
 }
 ?>

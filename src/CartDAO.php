cde5f71093af4942af97c3b7b858b6fb8d82b212
Tecnistico <arthurmegiolaro@gmail.com>
2023-04-24 17:32:38 -0400

    Cart DAO 1

--- src/CartDAO.php
+++ src/CartDAO.php
@@ -3,18 +3,19 @@
 class ProductDao
 {
     private PDO $db;
+    
     $cart = $_SESSION["cart"] ?? "";
-
     public function __construct(PDO $db)
     {
         $this->db = $db;
     }
 
     function add_item_to_cart(string $sku, int $qte) {
-        $cart[] = $sku => "$qte";
+        $cart[] = $sku => $qte;
     }
 
     function get_quantity_from_session(string $searchedSku) : int {
         $qte = $cart[$searchedSku];
         return intval($qte);
     }
+}    
